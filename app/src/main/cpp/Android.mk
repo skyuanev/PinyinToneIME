e65d2e72fa509cf57e170c51d5235c8d26299645
skyuanev <41504485+skyuanev@users.noreply.github.com>
2024-01-17 02:34:32 +1100

    Built with Android Studio 2023.1.1

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -27,6 +27,7 @@ LOCAL_C_INCLUDES += $(JNI_H_INCLUDE)
 LOCAL_MODULE := libjni_pinyinime
 #LOCAL_SHARED_LIBRARIES := libcutils libutils
 LOCAL_MODULE_TAGS := optional
+LOCAL_PRIVATE_PLATFORMS_APIS := true
 
 LOCAL_CFLAGS += -D_PLATFORM_ANDROID
 LOCAL_LDLIBS := -llog -landroid -lz
